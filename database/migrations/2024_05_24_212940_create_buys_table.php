34a80ee04f0d62a52ee482f33949bc0d83873ab6
Luis Beltran Condori <luisbeltrancondoricano@gmail.com>
2024-05-24 22:19:19 -0400

    crear migracion en clase falta ventas

---
+++ database/migrations/2024_05_24_212940_create_buys_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buys', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('buy_date');
+
+
+            $table->unsignedBigInteger('provider_id');
+            $table->foreign('provider_id')->references('id')->on('providers')->onDelete('cascade');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buys');
+    }
+};
